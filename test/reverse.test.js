9165af86feb164d242a0a3c449118e965bb3760b
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2021-02-07 12:27:07 +0100

    Update reverse.test.js

--- test/reverse.test.js
+++ test/reverse.test.js
@@ -1,9 +1,9 @@
 var nominatim = require('..');
 
-nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia'}, function(err, results) {
+nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia'}).then((results) => {
   var item = results.data[0];
   console.log(results);
-  nominatim.reverse({ lat: item.lat, lon: item.lon }, function(err, results) {
-  	console.log(results);
+  nominatim.reverse({ lat: item.lat, lon: item.lon }).then((resultsRev) => {
+  	console.log(resultsRev);
   });
 });
