25d008576c6723ce006a154b74ffe4870660a40c
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2021-02-09 21:46:47 +0100

    Incremented version to beta

--- test/reverse.test.js
+++ test/reverse.test.js
@@ -1,6 +1,8 @@
 var nominatim = require('..');
 
-nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia'}).then((results) => {
+console.log(process.env);
+
+nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia' }).then((results) => {
   var item = results.data[0];
   console.log(results);
   nominatim.reverse({ lat: item.lat, lon: item.lon }).then((resultsRev) => {
